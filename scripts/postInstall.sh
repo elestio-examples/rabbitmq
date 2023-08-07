83e0a109d65eeab18a52e94ece5f6e969c798ada
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-07 17:08:54 +0300

    adding elestio docker image

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,9 @@
 # nothing to do here ...
+set -o allexport; source .env; set +o allexport;
+
+line=$(grep "proxy_pass http://172.17.0.1:" /opt/elestio/nginx/conf.d/${DOMAIN}.conf)
+port=$(echo "$line" | sed -n 's/.*proxy_pass http:\/\/172\.17\.0\.1:\([0-9]\+\)\/.*/\1/p')
+sed -i "s~proxy_pass http://172.17.0.1:${port}/;~proxy_pass http://172.17.0.1:${port};~g" /opt/elestio/nginx/conf.d/${DOMAIN}.conf
+
+
+docker exec elestio-nginx nginx -s reload;
